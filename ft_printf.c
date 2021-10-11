53d8e20e877c7ec2333318c193b43115ada29780
Briar <86962846+TheBriar@users.noreply.github.com>
2021-10-11 18:32:44 +0200

    File creation

---
+++ ft_printf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+	Printf return the number of characters written, on failure it returns -1
+
+
+
+	c    char
+	s    char *
+	p    void * (to print pointer's adress)
+	d/i	 int
+	u    unsigned int
+	x/X	 number in hex(lcase/upcase)
+	%	percent sign
+
+	
+
+
+
+
+
+*/
+
+int ft_printf(const char *str, ...)
+{
+
+
+
+}
